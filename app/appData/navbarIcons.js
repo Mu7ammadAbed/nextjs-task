466d4c0c01ccd45eabc78a68a002ae67ac73bf1a
Muhammad Abdulhadi <mu7ammadabed@gmail.com>
2023-09-14 17:28:48 +0300

    exporting navbar data to external js file

---
+++ app/appData/navbarIcons.js
@@ -0,0 +1,38 @@
+const navbarIcons = [{
+    href: '#',
+    imageSrc: '/NavbarIcons/message.svg',
+    width: 24,
+    height: 24,
+    alternativeText: 'Messages Icon',
+    className: 'navbarlinks--item'
+  }, {
+    href: '#',
+    imageSrc: '/NavbarIcons/heart.svg',
+    width: 24,
+    height: 24,
+    alternativeText: 'Favorites Icon',
+    className: 'navbarlinks--item'
+  }, {
+    href: '#',
+    imageSrc: '/NavbarIcons/notification.svg',
+    width: 24,
+    height: 24,
+    alternativeText: 'Notifications Icon',
+    className: 'navbarlinks--item'
+  }, {
+    href: '#',
+    imageSrc: '/NavbarIcons/profile.svg',
+    width: 24,
+    height: 24,
+    alternativeText: 'Profile Icon',
+    className: 'navbarlinks--item'
+  }, {
+    href: '#',
+    imageSrc: '/NavbarIcons/cart.svg',
+    width: 24,
+    height: 24,
+    alternativeText: 'Cart Icon',
+    className: 'navbarlinks--item navbarlinks--lastitem'
+  }]
+
+  export default navbarIcons;
---
+++ app/appData/navbarLinks.js
@@ -0,0 +1,23 @@
+const navbarLinks = [{
+    value: "Home",
+    href: "#",
+    className: "navbarlinks--item navbarlinks--firstitem"
+  }, {
+    value: "Products",
+    href: "#",
+    className: "navbarlinks--item"
+  }, {
+    value: "Categories",
+    href: "#",
+    className: "navbarlinks--item"
+  }, {
+    value: "Offers",
+    href: "#",
+    className: "navbarlinks--item"
+  }, {
+    value: "Contact Us",
+    href: "#",
+    className: "navbarlinks--item"
+  }]
+
+  export default navbarLinks;
