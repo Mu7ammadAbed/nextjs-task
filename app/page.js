dff7a6c77c466ec436de1219113f9fdcc6c04ecc
Muhammad Abdulhadi <mu7ammadabed@gmail.com>
2023-09-14 03:51:44 +0300

    finishing navbar

--- app/page.js
+++ app/page.js
@@ -1,12 +1,8 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-import LanguageCurrency from './components/LanguageCurrency'
-import Logo from './components/Logo'
 import Navbar from './components/Navbar'
 
 export default function Home() {
   return (
-    <div className='language--currency'>
+    <div className='home--container'>
       <Navbar />
     </div>
   )
